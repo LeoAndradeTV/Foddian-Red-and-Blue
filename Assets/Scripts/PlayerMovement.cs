8f90d7e1fc607d68701a2c35d555435135c1bf0c
Leonardo de Andrade <leoandradetv@gmail.com>
2022-11-15 17:10:16 -0500

    Added animations
    Still needs a lot of work

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,11 +14,15 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector3 movePosition;
 
+    private Animator animator;
+
     private float horizontalInput;
     private float timeAtBeginningOfJump;
     private float timeAtEndingOfJump;
     private float jumpMod;
     private float timeDifference;
+    private float rotationSpeed = 800f;
+    private float isGroundedRange = 0.35f;
 
     private Vector3 savePosition;
 
@@ -30,6 +34,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
         savePosition = transform.position;
     }
 
@@ -61,9 +66,23 @@ public class PlayerMovement : MonoBehaviour
     private void GetMovementInput()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
+        if (horizontalInput != 0 && !Input.GetButton("Jump") && isGrounded)
+        {
+            animator.SetFloat("Speed_f", Mathf.Abs(horizontalInput));
+            animator.SetBool("Static_b", false);
+        } else
+        {
+            animator.SetFloat("Speed_f", 0);
+            animator.SetBool("Static_b", true);
+        }
+        if (horizontalInput != 0)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movePosition, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
         getJumpInput = Input.GetButtonDown("Jump");
-        movePosition = new Vector3(horizontalInput, 0f, 0f);
-        isGrounded = (Physics.Raycast(groundCheck.position, Vector3.down, 0.2f, groundLayer)) || (Physics.Raycast(groundCheck.position - new Vector3(1.75f, 0, 0), Vector3.down , 0.2f, groundLayer) || (Physics.Raycast(groundCheck.position - new Vector3(0.75f, 0, 0), Vector3.down, 0.2f, groundLayer)));
+        movePosition = new Vector3(-horizontalInput, 0f, 0f);
+        isGrounded = (Physics.Raycast(groundCheck.position, Vector3.down, isGroundedRange, groundLayer)) || (Physics.Raycast(groundCheck.position + new Vector3(0.7f, 0, 0), Vector3.down , isGroundedRange, groundLayer)) || (Physics.Raycast(groundCheck.position - new Vector3(0.7f, 0, 0), Vector3.down, isGroundedRange, groundLayer)) || (Physics.Raycast(groundCheck.position + new Vector3(0f, 0, 0.35f), Vector3.down, isGroundedRange, groundLayer)) || (Physics.Raycast(groundCheck.position - new Vector3(0.35f, 0, 0), Vector3.down, isGroundedRange, groundLayer));
     }
 
     private void CheckJump()
@@ -81,13 +100,16 @@ public class PlayerMovement : MonoBehaviour
             timeAtEndingOfJump = Time.time;
             jumpMod = 0.5f + (timeAtEndingOfJump - timeAtBeginningOfJump) * 1.05f;
             jumpMod = Mathf.Clamp(jumpMod, 0.5f, 1.5f);
-            rb.AddForce(((Vector3.up * jumpMod) + movePosition) * startJumpForce, ForceMode.Impulse);
+            Vector3 forceToAdd = ((Vector3.up * jumpMod) + movePosition) * startJumpForce;
+            rb.AddForce(forceToAdd, ForceMode.Impulse);
             if (isGrounded)
             {
                 timeDifference = 0f;
             }
         }
         
+        animator.SetBool("Grounded", isGrounded);
+
     }
 
     private void FixedUpdate()
@@ -95,6 +117,11 @@ public class PlayerMovement : MonoBehaviour
         if (isGrounded && !Input.GetButton("Jump"))
         {
             rb.AddForce(movePosition * moveSpeed, ForceMode.Force);
-        }  
+        }
+        if (horizontalInput == 0 && isGrounded)
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
     }
 }
